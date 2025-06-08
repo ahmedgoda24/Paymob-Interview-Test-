bc6e759d3f734af223e32784e0adcd490c52efca
Ahemd Goda <goda242424@gmail.com>
2025-06-08 20:33:23 +0300

    remove uncessary comment code

--- task1/serializers.py
+++ task1/serializers.py
@@ -2,18 +2,7 @@
 from rest_framework import serializers
 from .models import Product, Category
 
-# class CategorySerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Category
-#         fields = ['id', 'name']
 
-# class ProductSerializer(serializers.ModelSerializer):
-#     category = CategorySerializer()
-#     category_count = serializers.IntegerField(read_only=True)
-
-#     class Meta:
-#         model = Product
-#         fields = ['id', 'name', 'price', 'category', 'created_at', 'category_count']
 class CategorySerializer(serializers.ModelSerializer):
     
     class Meta:
